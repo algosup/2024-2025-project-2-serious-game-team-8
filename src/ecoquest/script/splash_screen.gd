f116614cffa6a30fdd9e41ed1df5265ac6f8634c
Marlier Thibaud <thibaud.marlier@algosup.com>
2024-11-22 14:05:12 +0100

    wip splash screen

---
+++ src/ecoquest/script/splash_screen.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property($CanvasLayer/HBoxContainer/Logo,"scale", Vector2(1,1), 1)
+	
