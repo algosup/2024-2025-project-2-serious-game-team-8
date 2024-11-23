31ce23c86ee273cf1c589bf702f65a3df11536ab
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-11-23 17:30:04 +0100

    Feat: creation of settings scene

--- src/ecoquest/scene/home_page.gd
+++ src/ecoquest/scene/home_page.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 func _on_texture_button_pressed() -> void:
-	print("button pressed")
+	pass
 
-
+var settings = preload("res://settings_page.tscn")
 func _on_setting_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().current_scene.add_child(settings)
