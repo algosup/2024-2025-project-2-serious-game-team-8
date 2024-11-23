8994f5961b68caa069d796b3294f8d478fb0c74b
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-11-23 18:58:57 +0100

    Fix: fix settings page
    The settings page can now be instantiated on top of the home page,  the close button also works.

--- src/ecoquest/scene/home_page.gd
+++ src/ecoquest/scene/home_page.gd
@@ -5,6 +5,6 @@ extends Node2D
 func _on_texture_button_pressed() -> void:
 	pass
 
-var settings = preload("res://settings_page.tscn")
+var settings = preload("res://scene/settings_page.tscn")
 func _on_setting_button_pressed() -> void:
-	get_tree().current_scene.add_child(settings)
+	add_child(settings.instantiate())
--- src/ecoquest/script/splash_screen.gd
+++ src/ecoquest/script/splash_screen.gd
@@ -16,8 +16,10 @@ func _ready() -> void:
 
 func _on_tween_finished() -> void:
 	$LoadingTime.start()
+	
+var home:PackedScene = preload("res://scene/home_page.tscn")
 
 func _on_loading_time_timeout() -> void:
-	var result = get_tree().change_scene_to_file("res://scene/home_page.tscn")
+	var result = get_tree().change_scene_to_packed(home)
 	if result != OK:
 		print("Failed to change scene: ", result)
