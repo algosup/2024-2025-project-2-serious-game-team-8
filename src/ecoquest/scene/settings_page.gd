7594c167c201a2d86e107ed081115a2ad6e60211
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-11-25 13:58:41 +0100

    Feat & fix: settings page
    Implementation of the feature: A button on the settings page redirecting to the repository
    
    Implementation of a fix of an issue where pressing the 'close' button would not display the proper texture.

--- src/ecoquest/scene/settings_page.gd
+++ src/ecoquest/scene/settings_page.gd
@@ -26,7 +26,12 @@ func _on_music_slider_value_changed(value: float) -> void:
 		bus_index,
 		linear_to_db(value)
 	)
-
+	
+### Close the settings page
 func _on_close_button_pressed() -> void:
 	# Clean up the node
 	queue_free()
+
+### Open the Github repository on a browser
+func _on_github_button_pressed() -> void:
+	OS.shell_open("https://github.com/algosup/2024-2025-project-2-serious-game-team-8")
