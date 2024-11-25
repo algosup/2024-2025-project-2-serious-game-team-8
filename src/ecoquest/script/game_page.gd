126abce626e3d516253a78f45fa8cfb2df856ad7
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-11-25 16:32:25 +0100

    Feat: timer can now be paused

--- src/ecoquest/script/game_page.gd
+++ src/ecoquest/script/game_page.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var is_paused: bool = false
+func _ready() -> void:
+	$CanvasLayer/Timer.start()
+func _process(delta: float) -> void:
+	var minutes = int($CanvasLayer/Timer.time_left / 60) # Get the minutes 
+	var seconds = int(fmod($CanvasLayer/Timer.time_left,  60)) # Get the seconds 
+	$CanvasLayer/TimerBg/TimerText.text = str(minutes)+':'+str(seconds)
+
 
 ### Close the settings page
 func _on_close_button_pressed() -> void:
@@ -8,6 +15,7 @@ func _on_close_button_pressed() -> void:
 
 var visibilityOn= load("res://resources/svg/visibility.png")
 var visibilityOff= load("res://resources/svg/no-visibility.png")
+
 ### Change the visibility icon's texture and toggle the visibility of the game controls on/off
 func _on_visibility_pressed() -> void:
 	if($CanvasLayer/GameControls.visible):
@@ -17,14 +25,14 @@ func _on_visibility_pressed() -> void:
 		$CanvasLayer/VisibilityControl/Visibility.texture_normal=visibilityOn
 		$CanvasLayer/GameControls.visible=true
 		
-	
-
 func _on_pause_button_pressed() -> void:
 	if is_paused == false: 
 		is_paused = true
 		$CanvasLayer/GameControls/PauseAnimation.play("pause")
+		$CanvasLayer/Timer.paused=true
 	else:  # If `is_paused` is true
 		is_paused = false
+		$CanvasLayer/Timer.paused=false
 		$CanvasLayer/GameControls/PauseAnimation.play("RESET")
 	
 	print(is_paused)
