2bf1e3454d6e7e02a4327aad75e04cf8c4a351fb
Marlier Thibaud <thibaud.marlier@algosup.com>
2024-11-26 18:37:14 +0100

    adding correct and incorrect popup for the code page

--- src/ecoquest/scene/code_page.gd
+++ src/ecoquest/scene/code_page.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 func _ready() -> void:
 	$CanvasLayer/TimerControl/TimerBg/TimerText.modulate = get_parent().get_child(0).get_node("TimerControl").get_node("TimerBg").get_node("TimerText").modulate 
 
@@ -11,6 +10,7 @@ var input1= "CanvasLayer/CodeControl/Input1/Label"
 var input2= "CanvasLayer/CodeControl/Input2/Label"
 var input3= "CanvasLayer/CodeControl/Input3/Label"
 var input4= "CanvasLayer/CodeControl/Input4/Label"
+
 func _on_input_pressed(buttonID: int) -> void:
 	match buttonID:
 		1:
@@ -57,6 +57,7 @@ func _on_input_pressed(buttonID: int) -> void:
 
 func _on_enter_button_pressed() -> void:
 	if(get_node(input1).text=="4" && get_node(input2).text=="7" && get_node(input3).text=="3" && get_node(input4).text=="2"):
-		print("success")
+		add_child(Global.win_page.instantiate())
 	else:
-		print("failure")
+		add_child(Global.error_page.instantiate())
+		
--- src/ecoquest/script/global.gd
+++ src/ecoquest/script/global.gd
@@ -3,6 +3,9 @@ extends Node2D
 var settings = preload("res://scene/settings_page.tscn")
 var chapter_selection = preload("res://scene/chapters_selection.tscn")
 var code_page = preload("res://scene/code_page.tscn")
+var win_page = preload("res://scene/code_win_page.tscn")
+var game_page = preload("res://scene/game_page.tscn")
+var error_page = preload("res://scene/code_error_page.tscn")
 
 var screen_size: Vector2 = Vector2()  # Initialize screen_size as a Vector2
 var music_slider_value: float = 1.0  # Default volume value
