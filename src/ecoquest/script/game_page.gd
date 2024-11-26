2988ef6924adba9aabb12bd8e848b13de17bbf69
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-11-26 15:59:31 +0100

    Feat: Game page exceeding time
    Implementation of the feature allowing the timer to increase in time once 15 minutes have passed. The background of the game also changes but is not responsive.

--- src/ecoquest/script/game_page.gd
+++ src/ecoquest/script/game_page.gd
@@ -1,64 +1,76 @@
 extends Node2D
 
 var is_paused: bool = false
-var initial_time: float = 900.0  # Initial time in seconds (15 minutes)
+
+var minutes: int
+var seconds: int
+
 
 func _ready() -> void:
-	$CanvasLayer/Timer.wait_time = initial_time  # Set the timer's countdown time
-	$CanvasLayer/Timer.start()
-	
+	minutes = 0
+	seconds = 0
+
+
 func _process(_delta: float) -> void:
-	var minutes = int($CanvasLayer/Timer.time_left / 60)  # Get the minutes
-	var seconds = int(fmod($CanvasLayer/Timer.time_left, 60))  # Get the seconds
-	$CanvasLayer/TimerControl/TimerBg/TimerText.text = str(minutes)+':'+str(seconds)
-	_removing_leaves()
-	time_low_warning()
+	if $CanvasLayer/Timer.is_stopped() == false && $CanvasLayer/Timer.time_left >= 1:
+		minutes = int($CanvasLayer/Timer.time_left / 60)  # Get the minutes
+		seconds = int(fmod($CanvasLayer/Timer.time_left, 60))  # Get the seconds
+		_removing_leaves()
+		time_low_warning()
+	elif $CanvasLayer/IncrementTimer.is_stopped():
+		$CanvasLayer/IncrementTimer.start()
+
+	$CanvasLayer/TimerControl/TimerBg/TimerText.text = "+%02d:%02d" % [minutes, seconds]
 
 
 ### Close the settings page
 func _on_close_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/home_page.tscn")
 
-var visibilityOn= load("res://resources/svg/visibility.png")
-var visibilityOff= load("res://resources/svg/no-visibility.png")
+
+var visibilityOn = load("res://resources/svg/visibility.png")
+var visibilityOff = load("res://resources/svg/no-visibility.png")
+
 
 ### Change the visibility icon's texture and toggle the visibility of the game controls on/off
 func _on_visibility_pressed() -> void:
-	if($CanvasLayer/GameControls.visible):
-		$CanvasLayer/VisibilityControl/Visibility.texture_normal=visibilityOff
-		$CanvasLayer/GameControls.visible=false
+	if $CanvasLayer/GameControls.visible:
+		$CanvasLayer/VisibilityControl/Visibility.texture_normal = visibilityOff
+		$CanvasLayer/GameControls.visible = false
 	else:
-		$CanvasLayer/VisibilityControl/Visibility.texture_normal=visibilityOn
-		$CanvasLayer/GameControls.visible=true
-		
-	
+		$CanvasLayer/VisibilityControl/Visibility.texture_normal = visibilityOn
+		$CanvasLayer/GameControls.visible = true
+
 
 func _on_pause_pressed() -> void:
-	if is_paused == false: 
+	if is_paused == false:
 		is_paused = true
 		$CanvasLayer/GameControls/PauseAnimation.play("pause")
-		$CanvasLayer/Timer.paused=true
+		$CanvasLayer/Timer.paused = true
 
-	else:  
+	else:
 		is_paused = false
-		$CanvasLayer/Timer.paused=false
+		$CanvasLayer/Timer.paused = false
 		$CanvasLayer/GameControls/PauseAnimation.play("RESET")
-		
-func _removing_leaves() -> void:
-	if $CanvasLayer/Timer.time_left < 720:
-		$CanvasLayer/BackgroundControl/leaf5.visible = false
-	if $CanvasLayer/Timer.time_left < 540:
-		$CanvasLayer/BackgroundControl/leaf4.visible = false
-	if $CanvasLayer/Timer.time_left < 360:
-		$CanvasLayer/BackgroundControl/leaf3.visible = false
-	if $CanvasLayer/Timer.time_left < 180:
-		$CanvasLayer/BackgroundControl/leaf2.visible = false
-	if $CanvasLayer/Timer.time_left <= 0:
-		$CanvasLayer/BackgroundControl/leaf1.visible = false
 
-func time_low_warning() -> void: 
+
+func _removing_leaves() -> void:
+	if $CanvasLayer/Timer.time_left < 720 && $CanvasLayer/BackgroundControl/LeafFiveControl/leaf5.visible:
+		$CanvasLayer/BackgroundControl/LeafFiveControl/leaf5.visible = false
+	if $CanvasLayer/Timer.time_left < 540 && $CanvasLayer/BackgroundControl/LeafFourControl/leaf4.visible:
+		$CanvasLayer/BackgroundControl/LeafFourControl/leaf4.visible = false
+	if $CanvasLayer/Timer.time_left < 360 && $CanvasLayer/BackgroundControl/LeafThreeControl/leaf3.visible:
+		$CanvasLayer/BackgroundControl/LeafThreeControl/leaf3.visible = false
+	if $CanvasLayer/Timer.time_left < 180 && $CanvasLayer/BackgroundControl/LeafTwoControl/leaf2.visible:
+		$CanvasLayer/BackgroundControl/LeafTwoControl/leaf2.visible = false
+	if $CanvasLayer/Timer.time_left <= 0 && $CanvasLayer/BackgroundControl/LeafOneControl/leaf1.visible:
+		$CanvasLayer/BackgroundControl/LeafOneControl/leaf1.visible = false
+
+
+func time_low_warning() -> void:
 	if $CanvasLayer/Timer.time_left <= 120:
 		$CanvasLayer/TimerControl/TimerBg/TimerText.modulate = Color(1, 0, 0)  # Set text to red
+
 
 func _on_code_pressed() -> void:
 	add_child(Global.code_page.instantiate())
@@ -66,3 +78,15 @@ func _on_code_pressed() -> void:
 
 func _on_setting_button_pressed() -> void:
 	add_child(Global.settings.instantiate())
+
+
+func _on_timer_timeout() -> void:
+	$CanvasLayer/BackgroundControl/TextureRect.texture = load("res://resources/background/looseScreen2.png")
+
+
+func _on_increment_timer_timeout() -> void:
+	seconds+=1
+	if(seconds == 60):
+		seconds=0
+		minutes+=1
+		
