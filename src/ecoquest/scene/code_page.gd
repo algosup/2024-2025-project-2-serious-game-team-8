634c17c914a29c293034a7526e602f7eba244434
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-11-26 13:51:55 +0100

    Feat: code page implementation
    Implementation of the code page for the tutorial, the code to enter still has to be defined

--- src/ecoquest/scene/code_page.gd
+++ src/ecoquest/scene/code_page.gd
@@ -1,46 +1,65 @@
 extends Node2D
 
 
+func _process(_delta: float) -> void:
+	var minutes = int(get_parent().get_node("CanvasLayer").get_node("Timer").time_left / 60)  # Get the minutes
+	var seconds = int(fmod(get_parent().get_node("CanvasLayer").get_node("Timer").time_left, 60))  # Get the seconds
+	$CanvasLayer/TimerControl/TimerBg/TimerText.text = str(minutes) + ":" + str(seconds)
+
 
 func _on_return_button_pressed() -> void:
 	queue_free()
 
+var input1= "CanvasLayer/CodeControl/Input1/Label"
+var input2= "CanvasLayer/CodeControl/Input2/Label"
+var input3= "CanvasLayer/CodeControl/Input3/Label"
+var input4= "CanvasLayer/CodeControl/Input4/Label"
+func _on_input_pressed(buttonID: int) -> void:
+	match buttonID:
+		1:
+			if(int(get_node(input1).text)<9):
+				get_node(input1).text = str(int(get_node(input1).text) + 1)
+			else:
+				get_node(input1).text = "0"
+		2:
+			if(int(get_node(input1).text)>0):
+				get_node(input1).text = str(int(get_node(input1).text) - 1)
+			else:
+				get_node(input1).text = "9"
+		3:
+			if(int(get_node(input2).text)<9):
+				get_node(input2).text = str(int(get_node(input2).text) + 1)
+			else:
+				get_node(input2).text = "0"
+		4:
+			if(int(get_node(input2).text)>0):
+				get_node(input2).text = str(int(get_node(input2).text) - 1)
+			else:
+				get_node(input2).text ="9"
+		5:
+			if(int(get_node(input3).text)<9):
+				get_node(input3).text = str(int(get_node(input3).text) + 1)
+			else:
+				get_node(input3).text = "0"
+		6:
+			if(int(get_node(input3).text)>0):
+				get_node(input3).text = str(int(get_node(input3).text) - 1)
+			else:
+				get_node(input3).text = "9"
+		7:
+			if(int(get_node(input4).text)<9):
+				get_node(input4).text = str(int(get_node(input4).text) + 1)
+			else:
+				get_node(input4).text = "0"
+		8:
+			if(int(get_node(input4).text)>0):
+				get_node(input4).text = str(int(get_node(input4).text) - 1)
+			else:
+				get_node(input4).text = "9"
 
-func _on_button_one_pressed() -> void:
-	print("one pressed")
 
-
-func _on_button_two_pressed() -> void:
-	print("two pressed")
-
-
-func _on_button_three_pressed() -> void:
-	print("three pressed")
-
-
-func _on_button_four_pressed() -> void:
-	print("four pressed")
-
-
-func _on_button_five_pressed() -> void:
-	print("five pressed")
-
-
-func _on_button_six_pressed() -> void:
-	print("six pressed")
-
-
-func _on_button_seven_pressed() -> void:
-	print("seven pressed")
-
-
-func _on_button_eight_pressed() -> void:
-	print("eight pressed")
-
-
-func _on_button_nine_pressed() -> void:
-	print("nine pressed")
-
-
-func _on_enter_pressed() -> void:
-	print("enter pressed")
+func _on_enter_button_pressed() -> void:
+	if(get_node(input1).text=="1" && get_node(input2).text=="2" && get_node(input3).text=="3" && get_node(input4).text=="4"):
+		print("success")
+	else:
+		print("failure")
