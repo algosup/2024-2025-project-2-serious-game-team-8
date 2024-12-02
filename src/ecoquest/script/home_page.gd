e9bf404e3610bb1719bb8bffbc3602a214938169
Marlier Thibaud <thibaud.marlier@algosup.com>
2024-12-02 16:12:18 +0100

    FEAT: Added a fade for the home music

--- src/ecoquest/script/home_page.gd
+++ src/ecoquest/script/home_page.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready() -> void:
+	fade_in_music()
+
 
 func _on_texture_button_pressed() -> void:
 	add_child(Global.chapter_selection.instantiate())
@@ -8,3 +11,8 @@ func _on_texture_button_pressed() -> void:
 
 func _on_setting_button_pressed() -> void:
 	add_child(Global.settings.instantiate())
+
+func fade_in_music():
+	var tween = create_tween()
+	tween.tween_property($MenuMusic, "volume_db", 0, 3.0)  
+	$MenuMusic.play()
