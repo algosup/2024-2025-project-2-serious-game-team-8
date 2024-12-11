fb274bc00c3acb252256f1851477c08ef9a680f5
Marlier Thibaud <thibaud.marlier@algosup.com>
2024-12-11 11:59:48 +0100

    FEAT: Modified multiple pop-ups
    - Added story pop-up upon finishing the tutorial
    - Added instructions after finishing the tutorial
    - Added the framework for the enigma two pop-ups

--- src/ecoquest/script/code_win_page.gd
+++ src/ecoquest/script/code_win_page.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+var click: int = 1
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			queue_free()
+func _ready() -> void:
+	$CanvasLayer/Congratulations/CongratulationsControl.visible = true
+	$CanvasLayer/StoryPage/StoryControl.visible = true
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		if click == 1:
+			$CanvasLayer/StoryPage/StoryControl.visible = false
+			$CanvasLayer/Congratulations/CongratulationsControl.visible = true
+			click += 1  # Increment click properly
+		elif click == 2:
+			queue_free()  # Free the current scene
 			get_tree().change_scene_to_file("res://scene/home_page.tscn")
