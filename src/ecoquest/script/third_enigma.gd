0c21119a42c93e239fc0f85434f9ebd31468fc00
aurelienfernandez <aurelien.fernandez@algosup.com>
2024-12-12 15:30:58 +0100

    Fix: stop timer after winning chapter 1

--- src/ecoquest/script/third_enigma.gd
+++ src/ecoquest/script/third_enigma.gd
@@ -4,33 +4,40 @@ var code = ""
 
 var sfx_player: AudioStreamPlayer
 
+
 func _ready() -> void:
 	sfx_player = $SFX
+
 
 # Function to play the digicode sound effect
 func play_digicode_sfx() -> void:
 	sfx_player.stream = load("res://resources/musics/sound_effects/digicode.mp3")
 	sfx_player.play()
 
+
 # Function to play the correct sound effect
 func play_correct_sfx() -> void:
 	sfx_player.stream = load("res://resources/musics/sound_effects/correct.mp3")
 	sfx_player.play()
+
 
 # Function to play the incorrect sound effect
 func play_incorrect_sfx() -> void:
 	sfx_player.stream = load("res://resources/musics/sound_effects/incorrect.mp3")
 	sfx_player.play()
 
+
 func _on_return_button_pressed() -> void:
 	queue_free()
-	
+
+
 func _on_button_pressed(number: int) -> void:
 	play_digicode_sfx()
-	if (code.length() < 4):
+	if code.length() < 4:
 		code = code + str(number)
-	if (code.length() == 4):
-		if (code == "4132"):
+	if code.length() == 4:
+		if code == "4132":
+			get_parent().is_chapter_finished = true
 			play_correct_sfx()
 			$CanvasLayer/DigicodeControl/DigicodeFeedback.self_modulate = Color(0, 0.988, 0)
 			await get_tree().create_timer(0.15).timeout  # Wait for the duration
