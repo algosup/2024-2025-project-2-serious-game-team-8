453794b88d1658a4f1ee4b14069cc861838f258b
Marlier Thibaud <thibaud.marlier@algosup.com>
2024-12-13 10:29:58 +0100

    CHORE: Commenting code
    - chapter selection
    - code_page
    - removed unused scenes

--- src/ecoquest/script/chapters_selection.gd
+++ src/ecoquest/script/chapters_selection.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-@export var image_resource: Texture
+## This script is directly linked to the chapter selection page, it serves to navigate in the app
+## basic checking for chapters locking/unlocking and chapter initialization/selection.
+
+@export var image_resource: Texture # exporting Backgroudn texture to change it dynamically
 
 
 func _ready() -> void:
-	get_parent().get_node("TransitionLayer")
+	get_parent().get_node("TransitionLayer") # initializing transition
 
 	# Set the initial image on the specified node
 	var chapter_two_image = $CanvasLayer/ChapterTwoImgControl/ChapterTwoImage
@@ -13,53 +16,61 @@ func _ready() -> void:
 	Global.opened_hint_1_tutorial = false
 	Global.opened_hint_2_tutorial = false
 
-func _process(_delta: float) -> void:
-	pass
 
 func _on_setting_button_pressed() -> void:
+	# Transitions before moving to the setting page
 	get_parent().get_node("TransitionLayer").visible = true
 	await get_parent().get_node("TransitionLayer")._transition()
 	get_parent().add_child(Global.settings.instantiate())
 
 func _change_image_resource() -> void: 
 	if Global.beat_chapter1 == true: 
-		# Update the texture of the node when the condition is met
+		# Update the texture of the chapter image if it has been unlocked
 		var new_image_chapter_two_texture = load("res://resources/chapterImages/chapter2unlocked.png")
 		var image_node = $CanvasLayer/ChapterTwoImgControl/ChapterTwoImage
 		image_node.texture = new_image_chapter_two_texture
 
+# Tutorial image button
 func _on_chapter_one_button_pressed() -> void:
-	Global.current_chapter = 0
+	# Launch tutorial
+	Global.current_chapter = 0 # set the current chapter index to 0, tells the state machine which codes, hints and timer to use
 	add_child(Global.confirmation_tutorial.instantiate())
 
-
+# chapter one image button
 func _on_chapter_two_button_pressed() -> void:
-	Global.current_chapter = 1
+	# Launch chapter one
+	Global.current_chapter = 1 # set the current chapter index to 1, tells the state machine which codes, hints and timer to use
+	
+	# This checks whether the chapter is locked or not, and adds a pop-up accordingly
 	if Global.beat_chapter1 == false: 
 		var locked_instance = Global.chapter_locked.instantiate()
 		add_child(locked_instance)
 	elif Global.beat_chapter1 == true:
+		# Sends to the confirmation pop-up to start the level
 		add_child(Global.confirmation_chapter1.instantiate())
 
 
-
+# chapter two image button
 func _on_chapter_three_button_pressed() -> void:
+	# This checks whether the chapter is locked or not, and adds a pop-up accordingly
 	if Global.beat_chapter2 == false:
 		var locked_instance = Global.chapter_locked.instantiate()
 		add_child(locked_instance)
 	elif Global.beat_chapter2 == true: 
+		# Temporary pop-up showing as the story isn't yet available, this will change when chatper 2 is added.
 		var popup = Global.popup_page.instantiate()
 		popup.text= "You've beaten the game for now. \nOur team is working on a new chapter. \nstay tuned and come back later"
 		popup.color= Color(1,1,1)
 		add_child(popup)
 
-
+# This function runs whenever the the close button is pressed
 func _on_close_button_pressed() -> void:
+	# calls the transition first
 	get_parent().get_node("TransitionLayer").visible = true
 	await get_parent().get_node("TransitionLayer")._transition()
-	# Clean up the node
+	# Then cleans it up
 	queue_free()
 
-
+# This functions calls the transition whenever the node exits the tree, this fixes some issues with the transition
 func _on_tree_exiting() -> void:
 	await get_parent().get_node("TransitionLayer")._fade_in()
--- src/ecoquest/script/code_page.gd
+++ src/ecoquest/script/code_page.gd
@@ -1,21 +1,30 @@
 extends Node2D
 
-var sfx_player: AudioStreamPlayer
+## This script is linked to the code page (Tutorial's last enigma), this script does the checking for
+## the enigma, initializes various codes, manage user input and plays SFX.
+
+var sfx_player: AudioStreamPlayer # initializing the audio stream player of the page for easier  usage
 
 
 func _ready() -> void:
+	# The line below serves at retrieving the timer and its current time to initialize the time on a
+	# child of the game page
 	$CanvasLayer/TimerControl/TimerBg/TimerText.modulate = get_parent().get_child(0).get_node("TimerControl").get_node("TimerBg").get_node("TimerText").modulate
-	sfx_player = $SFX
 	
+	# Giving the sfx player node
+	sfx_player = $SFX
 
+# Function that frees the screen back to the parent when pressed
 func _on_return_button_pressed() -> void:
 	queue_free()
 
+# Variables for storing user inputs
 var input1 = "CanvasLayer/CodeControl/Input1/Label"
 var input2 = "CanvasLayer/CodeControl/Input2/Label"
 var input3 = "CanvasLayer/CodeControl/Input3/Label"
 var input4 = "CanvasLayer/CodeControl/Input4/Label"
 
+# Storing the different codes, for respectively tutorial, chapter 1, chapter2
 var codes=["3426","1443","1566"]
 
 # Function to play the incorrect sound effect
@@ -23,19 +32,25 @@ func play_incorrect_sfx() -> void:
 	sfx_player.stream = load("res://resources/musics/sound_effects/windowserror.mp3")
 	sfx_player.play()
 
+# Function to handle user input
 func _on_input_pressed(buttonID: int) -> void:
+	# match case to handle addition and substraction via buttons
 	match buttonID:
-		1:
+		# First register
+		1:  # If the button at the top has been pressed adds 1 to the register
 			if int(get_node(input1).text) < 9:
 				get_node(input1).text = str(int(get_node(input1).text) + 1)
 			else:
+				# if above 9, goes back to 0
 				get_node(input1).text = "0"
-		2:
+		2:  # If the button at the bottom has been pressed substracts 1 to the register
 			if int(get_node(input1).text) > 0:
 				get_node(input1).text = str(int(get_node(input1).text) - 1)
 			else:
+				# if below 0, sets to 9
 				get_node(input1).text = "9"
-		3:
+		# Second Register
+		3:  # Same logic as the register above
 			if int(get_node(input2).text) < 9:
 				get_node(input2).text = str(int(get_node(input2).text) + 1)
 			else:
@@ -45,6 +60,7 @@ func _on_input_pressed(buttonID: int) -> void:
 				get_node(input2).text = str(int(get_node(input2).text) - 1)
 			else:
 				get_node(input2).text = "9"
+		# Third Register
 		5:
 			if int(get_node(input3).text) < 9:
 				get_node(input3).text = str(int(get_node(input3).text) + 1)
@@ -55,6 +71,7 @@ func _on_input_pressed(buttonID: int) -> void:
 				get_node(input3).text = str(int(get_node(input3).text) - 1)
 			else:
 				get_node(input3).text = "9"
+		# Fourth Register
 		7:
 			if int(get_node(input4).text) < 9:
 				get_node(input4).text = str(int(get_node(input4).text) + 1)
@@ -66,22 +83,33 @@ func _on_input_pressed(buttonID: int) -> void:
 			else:
 				get_node(input4).text = "9"
 
-
+# This function runs eveytime the enter button is pressed to check if the code
+# entered is the right one. 
 func _on_enter_button_pressed() -> void:
+	# stores in the variable the text of each input
 	var answer = get_node(input1).text + get_node(input2).text + get_node(input3).text + get_node(input4).text
+	
+	# checks if the answer is the correct one for the chapter selected
 	if str(answer) == codes[Global.current_chapter]:
 		add_child(Global.win_page.instantiate())
+		# set the chapter finished boolean to true, this stops the timer
 		get_parent().is_chapter_finished = true
+		# sets the global variable to true, this changes the menu selection page
+		# and unlock the chapter for the user
 		Global.beat_chapter1 = true
+		# Save the state of completion to not lose progress
 		Global.save_game()
 	else:
 		play_incorrect_sfx()
+		# instantiate the pop-up
 		var popup = Global.popup_page.instantiate()
+		# set the text of the pop-up dynamically
 		popup.text= "There was an error. \nThis was the wrong password. \n-1 minute to the timer"
 		popup.color= Color(1,0,0)
 		add_child(popup)
 		var time_left = get_parent().get_child(0).get_node("Timer").time_left
 		get_parent().remove_time()
+		# remove 60 seconds to the timer as the user has made an error
 		if time_left <= 60:
 			$CanvasLayer/TimerControl/TimerBg/TimerText.modulate =  Color(1, 0, 0)
 		
