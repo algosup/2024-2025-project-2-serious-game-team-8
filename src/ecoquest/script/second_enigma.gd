1ca535fd2d5e2ca13a8826cc29b28e05ed6400c4
Marlier Thibaud <thibaud.marlier@algosup.com>
2024-12-13 14:34:25 +0100

    FIX: Fixed time removal enigma two

--- src/ecoquest/script/second_enigma.gd
+++ src/ecoquest/script/second_enigma.gd
@@ -216,6 +216,7 @@ func _on_analyse_button_pressed() -> void:
 		# If any result is false, the combination doesn't exist
 		if typeof(input1_result) == TYPE_BOOL or typeof(input2_result) == TYPE_BOOL or typeof(input3_result) == TYPE_BOOL:
 			popup("This combination does not exist. \nYou lose one minute.")
+			get_parent().remove_time()
 		# If all results match, display the appropriate feedback
 		elif input1_result == input2_result and input1_result == input3_result:
 			match input1_result:
@@ -245,8 +246,10 @@ func _on_analyse_button_pressed() -> void:
 					popup("Even if this mix seems to reduce limestone in water, I wouldn't use it in my house... Phosphates only stimulate the growth of algae and bacteria in the water, but benzalkonium is very toxic to aquatic organisms and poorly biodegradable...")
 		else:
 			popup("This combination does not exist. \nYou lose one minute.")
+			get_parent().remove_time()
 	else:
 		popup("This combination does not exist. \nYou lose one minute.")
+		get_parent().remove_time()
 
 # Displays a popup message with the given text
 func popup(text):
